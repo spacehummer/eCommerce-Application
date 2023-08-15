a8fbf62b78e949bc3cd00a8f54953133e13fbdbe
Yuriy Lavrushin <yuriy.lavroushin@gmail.com>
2023-08-15 19:40:37 +0400

    feat: add enum for components html elements names.

---
+++ src/components_params/tags-enum.ts
@@ -0,0 +1,22 @@
+const enum ListTags {
+  /* General structure elements */
+  CONTAINER = 'div',
+  HEADER = 'header',
+  MAIN = 'main',
+  FOOTER = 'footer',
+  SECTION = 'section',
+  /* Text */
+  H1 = 'h1',
+  H2 = 'h2',
+  H3 = 'h3',
+  PARAGRAPH = 'p',
+  SPAN = 'span',
+  /* UI interactive */
+  LINK = 'a',
+  BUTTON = 'button',
+  INPUT = 'input',
+  /* Media */
+  IMG = 'img',
+}
+
+export default ListTags;
