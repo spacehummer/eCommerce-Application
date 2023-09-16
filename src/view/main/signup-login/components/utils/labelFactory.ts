260fca7b9e33b3f54ef0a8acf6a3e83010df9bbb
Davyd <wliorjroo@gmail.com>
2023-09-16 14:19:56 +0300

    fix: address form street regex
    fix: elements with same `id`

--- src/view/main/signup-login/components/utils/labelFactory.ts
+++ src/view/main/signup-login/components/utils/labelFactory.ts
@@ -3,16 +3,15 @@ import TagsEnum from '#src/components_params/tags-enum';
 
 export type LabelArgs = {
   textContent: string;
-  htmlFor: string;
+  htmlFor?: string;
   classList?: string[];
 };
 
 export default class LabelFactory {
-  public static default({ textContent, htmlFor, classList }: LabelArgs): HTMLLabelElement {
+  public static default({ textContent, classList }: LabelArgs): HTMLLabelElement {
     const label = document.createElement(TagsEnum.LABEL);
     label.classList.add(ClassesEnum.INPUT_LABEL);
     if (classList) label.classList.add(...classList);
-    label.htmlFor = htmlFor;
     label.textContent = textContent;
     return label;
   }
